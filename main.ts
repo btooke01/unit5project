7ee88188265d08e16a9a5a3afc7a2d0d9e54a5ae
btooke01 <89264069+btooke01@users.noreply.github.com>
2023-02-07 14:34:58 -0600

    added guy

--- main.ts
+++ main.ts
@@ -43,3 +43,21 @@ ESTABAN.setBounceOnWall(true)
 controller.player1.moveSprite(Ben)
 controller.player2.moveSprite(ESTABAN)
 pause(5000)
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f e e e e f f . . . . 
+    . . . f e e e f f e e e f . . . 
+    . . f f f f f 2 2 f f f f f . . 
+    . . f f e 2 e 2 2 e 2 e f f . . 
+    . . f e 2 f 2 f f 2 f 2 e f . . 
+    . . f f f 2 2 e e 2 2 f f f . . 
+    . f f e f 2 f e e f 2 f e f f . 
+    . f e e f f e e e e f e e e f . 
+    . . f e e e e e e e e e e f . . 
+    . . . f e e e e e e e e f . . . 
+    . . e 4 f f f f f f f f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
